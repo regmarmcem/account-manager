7112b1a91970c6d833c3da6f951c245fa1d98e3f
regmarmcem <tknktknktknktknktknk@gmail.com>
2023-05-10 00:41:29 +0900

    [add] settings for .env file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/regmarmcem/account-manager
 
 go 1.18
 
-require github.com/go-chi/chi v1.5.4
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(".env file not found")
+	}
 
 	r := chi.NewRouter()
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
